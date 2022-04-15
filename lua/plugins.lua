6d937ce1dbbb0213c6c97155a241a0aac3f75ba1
dindin12138 <din12138@outlook.com>
2022-04-15 23:21:20 +0800

    update nvim-treesitter.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -19,6 +19,8 @@ packer.startup({
     use("glepnir/dashboard-nvim")
     -- project
     use("ahmedkhalf/project.nvim")
+    -- treesitter
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
     -------------------- colorschemes --------------------
     -- tokyonight
