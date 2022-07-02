cfd30b9189d59d199f96ed4568008cb05c6f46a5
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-07-02 22:56:32 +0800

    feat: replace galaxyline with lualine

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -4,8 +4,33 @@ function config.catppuccin()
   vim.cmd('colorscheme catppuccin')
 end
 
-function config.galaxyline()
-  require('modules.ui.eviline')
+function config.lualine()
+  require("lualine").setup({
+    options = {
+      theme = "auto",
+      component_separators = { left = "|", right = "|" },
+      -- https://github.com/ryanoasis/powerline-extra-symbols
+      section_separators = { left = " ", right = "" },
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = { 'filename' },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { 'filename' },
+      lualine_x = { 'location' },
+      lualine_y = {},
+      lualine_z = {}
+    },
+    tabline = {},
+    extensions = { "nvim-tree", "toggleterm" },
+  })
 end
 
 function config.dashboard()
