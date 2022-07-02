a8ff41281f8b77faeef9008f13b05048bba5ebb1
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-07-02 22:49:33 +0800

    feat: update bufferline config

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -52,18 +52,104 @@ end
 function config.nvim_bufferline()
   require('bufferline').setup {
     options = {
-      modified_icon = '✥',
-      buffer_close_icon = '',
       always_show_bufferline = false,
+      mode = "buffers",
+      numbers = "ordinal",
+      -- Command to close Buffer moll/vim-bbye :Bdelete
+      close_command = "Bdelete! %d",
+      right_mouse_command = "Bdelete! %d",
+      left_mouse_command = "buffer %d",
+      middle_mouse_command = nil,
+      indicator_icon = '▎',
+      buffer_close_icon = '',
+      modified_icon = '✥',
+      close_icon = '',
+      left_trunc_marker = '',
+      right_trunc_marker = '',
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "File Explorer",
+          highlight = "Directory",
+          text_align = "center",
+          padding = 1,
+        },
+      },
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local s = " "
+        for e, n in pairs(diagnostics_dict) do
+          local sym = e == "error" and " " or (e == "warning" and " " or "")
+          s = s .. n .. sym
+        end
+        return s
+      end,
     }
   }
 end
 
 function config.nvim_tree()
   require("nvim-tree").setup({
-    disable_netrw = false,
-    hijack_cursor = true,
+    disable_netrw = true,
     hijack_netrw = true,
+    open_on_setup = false,
+    ignore_ft_on_setup = { "dashboard" },
+    open_on_tab = false,
+    hijack_cursor = true,
+    diagnostics = {
+      enable = false,
+      icons = { hint = "", info = "", warning = "", error = "" },
+    },
+    -- project plugin
+    update_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_cwd = true,
+      ignore_list = {},
+    },
+    system_open = { cmd = nil, args = {} },
+    git = { enable = true, ignore = true, timeout = 500 },
+    filters = { dotfiles = false, custom = {} },
+    view = {
+      width = 30,
+      height = 30,
+      side = 'left',
+      hide_root_folder = false,
+      number = false,
+      relativenumber = false,
+      signcolumn = 'yes',
+      mappings = {
+        custom_only = false,
+        list = {
+          -- Open a file or folder
+          { key = { "l", "o", "<CR>" }, action = "edit" },
+          -- Open the file in split screen
+          { key = "v", action = "vsplit" },
+          { key = "h", action = "split" },
+          -- Show hidden files
+          { key = "i", action = "toggle_git_ignored" },
+          { key = ".", action = "toggle_dotfiles" },
+          -- File operations
+          { key = "<F5>", action = "refresh" },
+          { key = "a", action = "create" },
+          { key = "d", action = "remove" },
+          { key = "r", action = "rename" },
+          { key = "x", action = "cut" },
+          { key = "y", action = "copy" },
+          { key = "p", action = "paste" },
+        },
+      },
+    },
+    trash = {
+      cmd = "trash",
+      require_confirm = true,
+    },
+    actions = {
+      open_file = {
+        resize_window = true,
+        quit_on_open = false,
+      },
+    },
   })
 end
 
