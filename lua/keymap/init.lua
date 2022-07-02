79412505dcace54eae78e899a1d582677ff7006f
dindin12138 <din12138@outlook.com>
2022-07-02 22:24:39 +0800

    style: format

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -1,24 +1,25 @@
 require('keymap.config')
 local keymap = require('core.keymap')
 local map = keymap.map
-local silent,noremap =keymap.silent,keymap.noremap
+local silent, noremap = keymap.silent, keymap.noremap
 local opts = keymap.new_opts
 local cmd = keymap.cmd
 
 -- usage of plugins
 map {
   -- packer
-  {'n','<Leader>pu',cmd('PackerUpdate'),opts(noremap,silent)},
-  {'n','<Leader>pi',cmd('PackerInstall'),opts(noremap,silent)},
-  {'n','<Leader>pc',cmd('PackerCompile'),opts(noremap,silent)},
+  { 'n', '<Leader>ps', cmd('PackerSync'), opts(noremap, silent) },
+  { 'n', '<Leader>pu', cmd('PackerUpdate'), opts(noremap, silent) },
+  { 'n', '<Leader>pi', cmd('PackerInstall'), opts(noremap, silent) },
+  { 'n', '<Leader>pc', cmd('PackerCompile'), opts(noremap, silent) },
   -- dashboard
-  {'n','<Leader>n',cmd('DashboardNewFile'),opts(noremap,silent)},
-  {'n','<Leader>ss',cmd('SessionSave'),opts(noremap,silent)},
-  {'n','<Leader>sl',cmd('SessionLoad'),opts(noremap,silent)},
+  { 'n', '<Leader>n', cmd('DashboardNewFile'), opts(noremap, silent) },
+  { 'n', '<Leader>ss', cmd('SessionSave'), opts(noremap, silent) },
+  { 'n', '<Leader>sl', cmd('SessionLoad'), opts(noremap, silent) },
   -- nvimtree
-  {'n','<Leader>e',cmd('NvimTreeToggle'),opts(noremap,silent)},
+  { 'n', '<Leader>e', cmd('NvimTreeToggle'), opts(noremap, silent) },
   -- Telescope
-  {'n','<Leader>b',cmd('Telescope buffers'),opts(noremap,silent)},
-  {'n','<Leader>fa',cmd('Telescope live_grep'),opts(noremap,silent)},
-  {'n','<Leader>ff',cmd('Telescope find_files'),opts(noremap,silent)},
+  { 'n', '<Leader>b', cmd('Telescope buffers'), opts(noremap, silent) },
+  { 'n', '<Leader>fa', cmd('Telescope live_grep'), opts(noremap, silent) },
+  { 'n', '<Leader>ff', cmd('Telescope find_files'), opts(noremap, silent) },
 }
