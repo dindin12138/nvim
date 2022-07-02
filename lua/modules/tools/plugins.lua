5c345baa8a673c8ecb08714358ad647645be3c0b
dindin12138 <din12138@outlook.com>
2022-07-02 23:55:48 +0800

    feat: update project.nvim

--- lua/modules/tools/plugins.lua
+++ lua/modules/tools/plugins.lua
@@ -9,6 +9,7 @@ plugin { 'nvim-telescope/telescope.nvim',
     { 'nvim-lua/plenary.nvim', opt = true },
     { 'nvim-telescope/telescope-fzy-native.nvim', opt = true },
     { 'LinArcX/telescope-env.nvim', opt = true },
-    { 'ahmedkhalf/project.nvim', config = conf.project, opt = true }
   }
 }
+
+plugin { 'ahmedkhalf/project.nvim', config = conf.project }
