d6f993a2ed81c244d1c306dbd2a1681a1a9da760
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-07-03 23:15:18 +0800

    feat: comment useless configuration

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -5,24 +5,24 @@ plugin {
   'neovim/nvim-lspconfig',
   -- used filetype to lazyload lsp
   -- config your language filetype in here
-  ft = { 'lua', 'rust', 'c', 'cpp' },
+  -- ft = { 'lua', 'rust', 'c', 'cpp' },
   config = conf.nvim_lsp
 }
 
-plugin {
-  'hrsh7th/nvim-cmp',
-  event = 'BufReadPre',
-  config = conf.nvim_cmp,
-  requires = {
-    { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-lspconfig' },
-    { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
-    { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
-    { 'saadparwaiz1/cmp_luasnip', after = "LuaSnip" }
-  },
-}
+-- plugin {
+--   'hrsh7th/nvim-cmp',
+--   event = 'BufReadPre',
+--   config = conf.nvim_cmp,
+--   requires = {
+--     { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-lspconfig' },
+--     { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
+--     { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+--     { 'saadparwaiz1/cmp_luasnip', after = "LuaSnip" }
+--   },
+-- }
 
-plugin {
-  "L3MON4D3/LuaSnip",
-  event = 'InsertEnter',
-  config = conf.lua_snip
-}
+-- plugin {
+--   "L3MON4D3/LuaSnip",
+--   event = 'InsertEnter',
+--   config = conf.lua_snip
+-- }
