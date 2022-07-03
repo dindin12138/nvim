54a7026521aac8e430f836f8013baa41bb272722
dindin12138 <din12138@outlook.com>
2022-07-04 00:06:05 +0800

    feat: Code completion configuration

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -9,11 +9,58 @@ function config.nvim_cmp()
 	local cmp = require("cmp")
 
 	cmp.setup({
-		preselect = cmp.PreselectMode.Item,
-		window = {
-			completion = cmp.config.window.bordered(),
-			documentation = cmp.config.window.bordered(),
-		}
+		-- Specify the snippet engine
+		snippet = {
+			-- REQUIRED - you must specify a snippet engine
+			expand = function(args)
+				-- vim.fn["vsnip#anonymous"](args.body)     -- For `vsnip` users.
+				require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+				-- vim.fn["UltiSnips#Anon"](args.body)      -- For `ultisnips` users.
+				-- require'snippy'.expand_snippet(args.body)-- For `snippy` users.
+			end,
+		},
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			-- { name = "vsnip" }, -- For vsnip users.
+			{ name = 'luasnip' }, -- For luasnip users.
+			-- { name = 'ultisnips' },--For ultisnips users.
+			-- { name = 'snippy' },-- For snippy users.
+		}, { { name = "buffer" }, { name = "path" } }),
+		mapping = cmp.mapping.preset.insert({
+			-- Occurrence of complements
+			["<A-.>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+			-- Cancel
+			["<A-,>"] = cmp.mapping({
+				i = cmp.mapping.abort(),
+				c = cmp.mapping.close()
+			}),
+			["<C-k>"] = cmp.mapping.select_prev_item(),
+			["<C-j>"] = cmp.mapping.select_next_item(),
+			["<CR>"] = cmp.mapping.confirm({
+				select = true,
+				behavior = cmp.ConfirmBehavior.Replace
+			}),
+			["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+			["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+		}),
+	})
+
+	-- / Find patterns using buffer sources
+	cmp.setup.cmdline("/", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = {
+			{ name = "buffer" },
+		},
+	})
+
+	-- : Use path and cmdline sources in command line mode
+	cmp.setup.cmdline(":", {
+		mapping = cmp.mapping.preset.cmdline(),
+		sources = cmp.config.sources({
+			{ name = "path" },
+		}, {
+			{ name = "cmdline" },
+		}),
 	})
 end
 
--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -9,20 +9,22 @@ plugin {
   config = conf.nvim_lsp
 }
 
--- plugin {
---   'hrsh7th/nvim-cmp',
---   event = 'BufReadPre',
---   config = conf.nvim_cmp,
---   requires = {
---     { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-lspconfig' },
---     { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
---     { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
---     { 'saadparwaiz1/cmp_luasnip', after = "LuaSnip" }
---   },
--- }
+plugin {
+  'hrsh7th/nvim-cmp',
+  -- event = 'BufReadPre',
+  config = conf.nvim_cmp,
+  requires = {
+    { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-lspconfig' },
+    { 'hrsh7th/cmp-path', after = 'nvim-cmp' },
+    { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' },
+    { 'hrsh7th/cmp-cmdline', after = 'nvim-cmp' },
+    { 'saadparwaiz1/cmp_luasnip', after = "LuaSnip" }
+  },
+}
 
--- plugin {
---   "L3MON4D3/LuaSnip",
---   event = 'InsertEnter',
---   config = conf.lua_snip
--- }
+plugin {
+  'L3MON4D3/LuaSnip',
+  after = "nvim-cmp",
+  -- config = conf.lua_snip
+  requires = 'rafamadriz/friendly-snippets'
+}
