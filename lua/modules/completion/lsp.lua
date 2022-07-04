1376a4e2a5e82d1931e14aee9e54bcd7455ea529
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-07-04 08:20:27 +0800

    feat: add diagnostic icon

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -83,3 +83,18 @@ require('lspconfig')['clangd'].setup {
         "--pretty",
     },
 }
+
+local diagnostic_icon = function()
+    vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        -- update_in_insert = true,
+    })
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+end
+
+diagnostic_icon()
