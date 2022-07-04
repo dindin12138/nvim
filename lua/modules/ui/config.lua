c282f7f9c535b25c90d5dab1f8e613976a12e72c
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-07-04 08:39:19 +0800

    feat: add plugin indent-blankline

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -197,4 +197,41 @@ function config.nvim_tree()
   })
 end
 
+function config.indent_blankline()
+  require("indent_blankline").setup({
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+    context_patterns = {
+      "class",
+      "function",
+      "method",
+      "element",
+      "^if",
+      "^while",
+      "^for",
+      "^object",
+      "^table",
+      "block",
+      "arguments",
+    },
+    -- :echo &filetype
+    filetype_exclude = {
+      "dashboard",
+      "packer",
+      "terminal",
+      "help",
+      "log",
+      "markdown",
+      "TelescopePrompt",
+      "lsp-installer",
+      "lspinfo",
+      "toggleterm",
+      "json",
+      "txt",
+    },
+    char = "▏",
+  })
+end
+
 return config
