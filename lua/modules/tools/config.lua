76990d4ba82c9aff8b3d19312a1cd1fb8d93e79b
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-07-04 23:45:05 +0800

    feat: add plugin autosave

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -103,4 +103,24 @@ function config.filetype()
     })
 end
 
+function config.autosave()
+    require("autosave").setup(
+        {
+            enabled = true,
+            execution_message = "AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"),
+            events = { "InsertLeave" },
+            conditions = {
+                exists = true,
+                filename_is_not = {},
+                filetype_is_not = {},
+                modifiable = true
+            },
+            write_all_buffers = false,
+            on_off_commands = true,
+            clean_command_line_interval = 0,
+            debounce_delay = 135
+        }
+    )
+end
+
 return config
