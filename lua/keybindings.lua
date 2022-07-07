de098659a79c2e21ba77d9bc63b9a91ac71c097b
dindin12138 <din12138@outlook.com>
2022-07-07 11:56:40 +0800

    feat: update keys

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -11,19 +11,12 @@ local opt = { noremap = true, silent = true }
 map("n", "s", "<nop>", opt)
 
 -- windows 分屏快捷键
--- 向右分屏
 map("n", "sl", ":vsp<CR>", opt)
--- 向下分屏
 map("n", "sj", ":sp<CR>", opt)
--- 切换为垂直分屏
 map("n", "sv", "<C-w>t<C-w>H", opt)
--- 切换为水平分屏
 map("n", "sh", "<C-w>t<C-w>K", opt)
--- 关闭当前
 map("n", "sc", "<C-w>c", opt)
--- 关闭其他
 map("n", "so", "<C-w>o", opt)
--- alt + hjkl  窗口之间跳转
 map("n", "<C-h>", "<C-w>h", opt)
 map("n", "<C-j>", "<C-w>j", opt)
 map("n", "<C-k>", "<C-w>k", opt)
@@ -32,13 +25,9 @@ map("n", "<C-l>", "<C-w>l", opt)
 -- 左右比例控制
 map("n", "<C-Left>", ":vertical resize -2<CR>", opt)
 map("n", "<C-Right>", ":vertical resize +2<CR>", opt)
--- map("n", "s,", ":vertical resize -20<CR>", opt)
--- map("n", "s.", ":vertical resize +20<CR>", opt)
 -- 上下比例
 map("n", "<C-Down>", ":resize +2<CR>", opt)
 map("n", "<C-Up>", ":resize -2<CR>", opt)
--- map("n", "sj", ":resize +10<CR>", opt)
--- map("n", "sk", ":resize -10<CR>", opt)
 -- 等比例
 map("n", "s=", "<C-w>=", opt)
 
@@ -64,8 +53,6 @@ map("v", "p", '"_dP', opt)
 -- 保存 退出
 map("n", "<C-s>", ":w<CR>", opt)
 map("n", "W", ":w<CR>", opt)
-map("n", "q", ":q<CR>", opt)
-map("n", "qq", ":q!<CR>", opt)
 map("n", "Q", ":qa!<CR>", opt)
 
 -- insert 模式下，跳到行首行尾
@@ -80,23 +67,16 @@ local pluginKeys = {}
 map("n", "<leader>ps", ":PackerSync<CR>", opt)
 map("n", "<leader>pu", ":PackerUpdate<CR>", opt)
 map("n", "<leader>pi", ":PackerInstall<CR>", opt)
-map("n", "<leader>pc", ":PackerClean<CR>", opt)
+map("n", "<leader>pc", ":PackerCompile<CR>", opt)
 
 -- nvim-tree
--- tt 键打开关闭tree
 map("n", "<leader>e", ":NvimTreeToggle<CR>", opt)
-map("n", "<leader>tt", ":NvimTreeFindFile<CR>", opt)
--- 列表快捷键
 pluginKeys.nvimTreeList = {
-	-- 打开文件或文件夹
 	{ key = { "l", "o", "<CR>" }, action = "edit" },
-	-- 分屏打开文件
 	{ key = "v", action = "vsplit" },
 	{ key = "h", action = "split" },
-	-- 显示隐藏文件
 	{ key = "i", action = "toggle_git_ignored" },
 	{ key = ".", action = "toggle_dotfiles" },
-	-- 文件操作
 	{ key = "<F5>", action = "refresh" },
 	{ key = "a", action = "create" },
 	{ key = "d", action = "remove" },
@@ -107,10 +87,7 @@ pluginKeys.nvimTreeList = {
 }
 
 -- bufferline
--- 左右Tab切换
--- map("n", "<C-p>", ":BufferLineCyclePrev<CR>", opt)
 map("n", "<C-n>", ":BufferLineCycleNext<CR>", opt)
--- 关闭
 --"moll/vim-bbye"
 map("n", "<leader>q", ":Bdelete!<CR>", opt)
 map("n", "<leader>x", ":BufferLinePickClose<CR>", opt)
@@ -126,25 +103,21 @@ map("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>", opt)
 map("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>", opt)
 
 -- Telescope
-map("n", "<C-f>", ":Telescope find_files<CR>", opt)
-map("n", "<C-o>", ":Telescope oldfiles<CR>", opt)
-map("n", "<C-g>", ":Telescope live_grep<CR>", opt)
-map("n", "<C-p>", ":Telescope projects<CR>", opt)
-map("n", "<C-e>", ":Telescope env<CR>", opt)
+map("n", "<Leader>ff", ":Telescope find_files<CR>", opt)
+map("n", "<Leader>fo", ":Telescope oldfiles<CR>", opt)
+map("n", "<Leader>fa", ":Telescope live_grep<CR>", opt)
+map("n", "<Leader>fp", ":Telescope projects<CR>", opt)
+map("n", "<Leader>fe", ":Telescope env<CR>", opt)
 -- Telescope 列表中 插入模式快捷键
 pluginKeys.telescopeList = {
 	i = {
-		-- 上下移动
 		["<C-j>"] = "move_selection_next",
 		["<C-k>"] = "move_selection_previous",
 		["<Down>"] = "move_selection_next",
 		["<Up>"] = "move_selection_previous",
-		-- 历史记录
 		["<C-n>"] = "cycle_history_next",
 		["<C-p>"] = "cycle_history_prev",
-		-- 关闭窗口
 		["<C-c>"] = "close",
-		-- 预览窗口上下滚动
 		["<C-u>"] = "preview_scrolling_up",
 		["<C-d>"] = "preview_scrolling_down",
 	},
@@ -152,73 +125,41 @@ pluginKeys.telescopeList = {
 
 -- lsp 回调函数快捷键设置
 pluginKeys.mapLSP = function(mapbuf)
-	-- rename
-	-- mapbuf("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opt)
-	mapbuf("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opt)
-	-- code action
-	-- mapbuf("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opt)
-	mapbuf("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opt)
-	-- go xx
-	-- mapbuf("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
-	-- mapbuf("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
-	-- mapbuf("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opt)
-	-- mapbuf("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
-	-- mapbuf("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
+	mapbuf("n", "gr", "<cmd>Lspsaga rename<CR>", opt)
+	mapbuf("n", "ga", "<cmd>Lspsaga code_action<CR>", opt)
 	mapbuf("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
 	mapbuf("n", "gh", "<cmd>Lspsaga hover_doc<cr>", opt)
-	mapbuf("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", opt)
-	-- diagnostic
-	-- mapbuf("n", "gp", "<cmd>lua vim.diagnostic.open_float()<CR>", opt)
-	-- mapbuf("n", "gk", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opt)
-	-- mapbuf("n", "gj", "<cmd>lua vim.diagnostic.goto_next()<CR>", opt)
-	mapbuf("n", "gp", "<cmd>Lspsaga show_line_diagnostics<CR>", opt)
+	mapbuf("n", "gl", "<cmd>Lspsaga lsp_finder<CR>", opt)
+	mapbuf("n", "ge", "<cmd>Lspsaga show_line_diagnostics<CR>", opt)
 	mapbuf("n", "gj", "<cmd>Lspsaga diagnostic_jump_next<cr>", opt)
 	mapbuf("n", "gk", "<cmd>Lspsaga diagnostic_jump_prev<cr>", opt)
 	mapbuf("n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opt)
-	-- 没用到
-	-- mapbuf('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opt)
-	-- mapbuf("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opt)
-	-- mapbuf('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opt)
-	-- mapbuf('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opt)
-	-- mapbuf('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opt)
-	-- mapbuf('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opt)
 end
 
 -- LSP
-map("n", "<leader>li", ":LspInstallInfo<CR>", opt)
+map("n", "<leader>li", ":LspInfo<CR>", opt)
 map("n", "<leader>lr", ":LspRestart<CR>", opt)
 
 -- nvim-cmp 自动补全
 pluginKeys.cmp = function(cmp)
 	return {
-		-- 出现补全
 		["<A-.>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		-- 取消
 		["<A-,>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		-- 上一个
 		["<C-k>"] = cmp.mapping.select_prev_item(),
-		-- 下一个
 		["<C-j>"] = cmp.mapping.select_next_item(),
-		-- 确认
 		["<CR>"] = cmp.mapping.confirm({
 			select = true,
 			behavior = cmp.ConfirmBehavior.Replace,
 		}),
-		-- 如果窗口内容太多，可以滚动
 		["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 	}
 end
 
 -- 自定义 toggleterm 3个不同类型的命令行窗口
--- <leader>tf 浮动
--- <leader>tv 右侧
--- <leader>th 下方
--- 特殊lazygit 窗口，需要安装lazygit
--- <leader>tg lazygit
 pluginKeys.mapToggleTerm = function(toggleterm)
 	vim.keymap.set({ "n", "t" }, "<leader>tf", toggleterm.toggle_float)
 	vim.keymap.set({ "n", "t" }, "<leader>tv", toggleterm.toggle_vertical)
@@ -226,5 +167,4 @@ pluginKeys.mapToggleTerm = function(toggleterm)
 	vim.keymap.set({ "n", "t" }, "<leader>tg", toggleterm.toggle_lazygit)
 end
 
---- 注意：放在最后
 return pluginKeys
