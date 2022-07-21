9a4849ab0dadefcddb4ebce92b989ede1ed773ab
dindin12138 <din12138@outlook.com>
2022-07-21 23:04:05 +0800

    feat: update auto-session config

--- lua/modules/tools/config.lua
+++ lua/modules/tools/config.lua
@@ -174,20 +174,19 @@ function config.gitsigns()
 end
 
 function config.auto_session()
-    local opts = {
+    require('auto-session').setup({
         log_level = 'info',
         auto_session_enable_last_session = false,
         auto_session_root_dir = vim.fn.stdpath('data') .. "/sessions/",
-        auto_session_enabled = false,
-        auto_save_enabled = nil,
+        auto_session_enabled = true,
+        auto_session_create_enabled = false,
+        auto_save_enabled = true,
         auto_restore_enabled = nil,
         auto_session_suppress_dirs = nil,
         auto_session_use_git_branch = nil,
         -- the configs below are lua only
         bypass_session_save_file_types = nil
-    }
-
-    require('auto-session').setup(opts)
+    })
 end
 
 return config
