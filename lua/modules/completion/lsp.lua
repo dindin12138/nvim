d4e467a75c37909056d13fce8d0d1bf7bf66597a
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-08-14 00:01:51 +0800

    fix: Automatically set up installed servers

--- lua/modules/completion/lsp.lua
+++ lua/modules/completion/lsp.lua
@@ -1,3 +1,5 @@
+local lsp_installer = require("nvim-lsp-installer")
+local lspconfig = require("lspconfig")
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
@@ -10,7 +12,32 @@ local lsp_flags = {
     -- This is the default in Nvim 0.7+
     debounce_text_changes = 150,
 }
-require('lspconfig')['sumneko_lua'].setup {
+
+lsp_installer.setup({
+    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer", "sumneko_lua" }
+    -- This setting has no relation with the `automatic_installation` setting.
+    ensure_installed = { 'sumneko_lua', 'clangd', 'rust_analyzer', 'pyright', 'bashls', 'jsonls' },
+
+    -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+    -- This setting has no relation with the `ensure_installed` setting.
+    -- Can either be:
+    --   - false: Servers are not automatically installed.
+    --   - true: All servers set up via lspconfig are automatically installed.
+    --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+    --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+    automatic_installation = false,
+    ui = {
+        -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+        border = "rounded",
+        icons = {
+            server_installed = "✓",
+            server_pending = "➜",
+            server_uninstalled = "✗"
+        }
+    }
+})
+
+lspconfig['sumneko_lua'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     settings = {
@@ -35,7 +62,7 @@ require('lspconfig')['sumneko_lua'].setup {
     },
 }
 
-require('lspconfig')['clangd'].setup {
+lspconfig['clangd'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
     args = {
@@ -56,6 +83,30 @@ require('lspconfig')['clangd'].setup {
         "-j=12",
         "--pretty",
     },
+}
+
+lspconfig['pyright'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true
+            }
+        }
+    },
+}
+
+lspconfig['jsonls'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
+
+lspconfig['rust_analyzer'].setup {
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 
 local diagnostic_icon = function()
