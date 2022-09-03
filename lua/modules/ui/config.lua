8c61d9cd71c2ca88478b1c4c4864ad97cb5e2bed
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2022-09-03 22:42:45 +0800

    feat: delete useless config

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -134,16 +134,6 @@ function config.bufferline()
             right_mouse_command = "Bdelete! %d",
             left_mouse_command = "buffer %d",
             middle_mouse_command = nil,
-            -- indicator_icon = '▎',
-            indicator = {
-                style = 'icon',
-                icon = '▎'
-            },
-            buffer_close_icon = '',
-            modified_icon = '✥',
-            close_icon = '',
-            left_trunc_marker = '',
-            right_trunc_marker = '',
             offsets = {
                 {
                     filetype = "NvimTree",
