e77309d085437c248df72420e6057746cd8c2961
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2023-10-18 09:13:47 +0800

    feat: update lua_ls config

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -45,17 +45,13 @@ lspconfig.lua_ls.setup {
     settings = {
         Lua = {
             runtime = {
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                globals = { 'vim' },
+                version = 'LuaJIT'
             },
             workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
                 checkThirdParty = false,
-            },
-            telemetry = {
-                enable = false,
+                library = {
+                    vim.env.VIMRUNTIME
+                }
             }
         }
     }
