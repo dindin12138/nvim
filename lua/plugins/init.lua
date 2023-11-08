548536f810b8cf1118488884c75d2444549b52ce
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2023-11-08 21:42:22 +0800

    feat: change notify color to blue

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -36,6 +36,11 @@ return {
     event = "VeryLazy",
     opts = { stages = "static" },
     config = function(_, opts)
+      vim.cmd([[
+        highlight NotifyINFOBorder guifg=#89B4FA
+        highlight NotifyINFOIcon guifg=#89B4FA
+        highlight NotifyINFOTitle guifg=#89B4FA
+      ]])
       require("notify").setup(opts)
       vim.notify = require("notify")
     end,
