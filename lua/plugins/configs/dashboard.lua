457631b41c2921606f065b0b2e9c0b965465bf6c
dindin12138 <79844759+dindin12138@users.noreply.github.com>
2023-12-23 18:58:29 +0800

    refactor: Reconfiguring dashboard Settings

--- lua/plugins/configs/dashboard.lua
+++ lua/plugins/configs/dashboard.lua
@@ -1,89 +1,49 @@
-return {
+local logo = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+]]
+logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+local opts = {
   theme = "doom",
   config = {
-    header = {
-      [[]],
-      [[]],
-      [[]],
-      [[]],
-      [[]],
-      [[]],
-      [[]],
-      [[]],
-      [[███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
-      [[████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
-      [[██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
-      [[██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
-      [[██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
-      [[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-      [[]],
-      [[]],
-    },
-    center = {
-      {
-        icon = "  ",
-        icon_hl = "Title",
-        desc = "Projects                                ",
-        desc_hl = "String",
-        key = "p",
-        key_hl = "Number",
-        action = "Telescope projects",
+    header = vim.split(logo, "\n"),
+      -- stylua: ignore
+      center = {
+        { action = "Telescope projects", desc = "Projects", icon = "  ", key = "p" },
+        { action = "Telescope oldfiles", desc = "Recent files", icon = "  ", key = "o" },
+        { action = "Telescope find_files", desc = "Find Files", icon = "󰥨  ", key = "f" },
+        { action = "Lazy", desc = "Lazy", icon = "󰒲  ", key = "l" },
+        { action = "edit ~/.config/nvim/lua/plugins/init.lua", desc = "Config", icon = "  ", key = "c" },
+        { action = "qa", desc = "Quit", icon = "  ", key = "q" },
       },
-      {
-        icon = "  ",
-        icon_hl = "Title",
-        desc = "Recently Files                          ",
-        desc_hl = "String",
-        key = "o",
-        key_hl = "Number",
-        action = "Telescope oldfiles",
-      },
-      {
-        icon = "󰥨  ",
-        icon_hl = "Title",
-        desc = "Find Files                              ",
-        desc_hl = "String",
-        key = "f",
-        key_hl = "Number",
-        action = "Telescope find_files",
-      },
-      {
-        icon = "󰒲  ",
-        icon_hl = "Title",
-        desc = "Lazy                                    ",
-        desc_hl = "String",
-        key = "l",
-        key_hl = "Number",
-        action = "Lazy",
-      },
-      {
-        icon = "  ",
-        icon_hl = "Title",
-        desc = "Config                                  ",
-        desc_hl = "String",
-        key = "c",
-        key_hl = "Number",
-        action = "edit ~/.config/nvim/lua/core/lazy.lua",
-      },
-      {
-        icon = "  ",
-        icon_hl = "Title",
-        desc = "Quit                                    ",
-        desc_hl = "String",
-        key = "q",
-        key_hl = "Number",
-        action = "qa",
-      },
-    },
     footer = function()
       local stats = require("lazy").stats()
       local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-      return {
-        "",
-        "Talk is cheap. Show me the code.",
-        "",
-        "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
-      }
+      local foot = "\n"
+        .. "Talk is cheap. Show me the code."
+        .. "\n\n"
+        .. "⚡ Neovim loaded "
+        .. stats.loaded
+        .. "/"
+        .. stats.count
+        .. " plugins in "
+        .. ms
+        .. "ms"
+      return vim.split(foot, "\n")
     end,
   },
 }
+
+for _, button in ipairs(opts.config.center) do
+  button.desc = button.desc .. string.rep(" ", 40 - #button.desc)
+  button.icon_hl = "Title"
+  button.desc_hl = "String"
+  button.key_hl = "Number"
+end
+
+return opts
